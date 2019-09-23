7b4e37ed84424da452ee2bb9b39028675abf0d70
Lu Alumi <r2123b@gmail.com>
2019-09-23 13:33:35 +0800

    refactor EditorCoroutine.cs

--- EditorCoroutine.cs
+++ EditorCoroutine.cs
@@ -4,32 +4,34 @@ using UnityEditor;
 
 [InitializeOnLoad]
 public static class EditorCoroutine {
-  // Constructor
+  /// <summary>
+  ///  Coroutines to be executed.
+  /// </summary>
+  private static List<IEnumerator> CoroutineInProgress = new List<IEnumerator>();
+  static int currentExecute = 0;
+
   static EditorCoroutine() {
     EditorApplication.update += ExecuteCoroutine;
   }
-  
-  // This is the entry function
+
+  // This is my callable function
   public static IEnumerator StartCoroutine(IEnumerator newCorou) {
     CoroutineInProgress.Add(newCorou);
     return newCorou;
   }
 
-  /// <summary>
-  ///  Coroutine to execute. Manage by the EasyLocalization_script
-  /// </summary>
-  private static List<IEnumerator> CoroutineInProgress = new List<IEnumerator>();
-
-  static int currentExecute = 0;
   private static void ExecuteCoroutine() {
     if (CoroutineInProgress.Count <= 0) {
       //  Debug.LogWarning("ping");
       return;
     }
+
     // Debug.LogWarning("execute coroutine...");
 
     currentExecute = (currentExecute + 1) % CoroutineInProgress.Count;
+
     bool finish = !CoroutineInProgress[currentExecute].MoveNext();
+
     if (finish)
       CoroutineInProgress.RemoveAt(currentExecute);
   }
